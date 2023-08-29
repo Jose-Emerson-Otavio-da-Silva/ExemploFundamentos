28d43d3df82ce0bb7be74b4b035904da38a88bc9
emersonr8 <emersonr8@hotmail.com>
2023-08-28 21:45:37 -0300

    Inicio Curso DIO

---
+++ ExemploFundamentos/Program.cs
@@ -0,0 +1,39 @@
+﻿using ExemploFundamentos.Common.Models;
+
+Calculadora calculadora = new();
+
+calculadora.RaizQuadrada(9);
+
+// calculadora.Seno(30);
+// calculadora.Coseno(30);
+// calculadora.Tangente(30);
+
+
+
+// calculadora.Somar(10, 30);
+// calculadora.Subtrair(10,50);
+// calculadora.Multiplicar(15,45);
+// calculadora.Dividir(2,2);
+
+
+
+
+
+
+
+
+
+
+// DateTime dateAtual = DateTime.Now;
+
+// Console.WriteLine("Data e hora atual: " +
+// DateTime.Now.AddHours(3).AddMinutes(30));
+
+// string a = 5.ToString();
+Pessoas pessoas = new()
+{
+    Nome = "Emerson",
+    Idade = 41
+};
+
+pessoas.Apresentar();
